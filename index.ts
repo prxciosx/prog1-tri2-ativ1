b6e97520e75b6b1e99cb3bc6809b6509758cb75c
Nicolas Alves Freire <n1colas.4lveshn@gmail.com>
2026-05-13 14:35:40 -0300

    Arrumando Arquivo

---
+++ index.ts
@@ -0,0 +1,21 @@
+const server = Bun.serve({
+  routes: {},
+
+  async fetch(req) {
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+    const filePath = `./public${pathname}`;
+    const file = Bun.file(filePath);
+    const exists = await file.exists();
+
+    if (!exists)
+      return new Response("Not Found", { status: 404})
+    return new Response(file);
+
+    return new Response(file);
+
+  },
+
+});
+
+console.log(`Server running at ${server.url}`);
